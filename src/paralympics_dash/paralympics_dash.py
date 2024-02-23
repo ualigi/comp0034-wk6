d83d3de7de4f86c7ae356b8634abdc9711b6762e
ualigi <giulia.clini@gmail.com>
2024-02-23 14:54:52 +0000

    work through tutorial content

--- src/paralympics_dash/paralympics_dash.py
+++ src/paralympics_dash/paralympics_dash.py
@@ -1,10 +1,28 @@
 from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 
-app.layout = html.Div([
-    html.Div(children='Hello World')
-])
+meta_tags = [
+    {"name": "viewport", "content": "width=device-width, inital-scale=1"}
+]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets,
+           meta_tags=meta_tags)
+
+
+app.layout = dbc.Container(
+    html.Div([
+        html.H1("This is my HTML page"),
+        html.Div([
+            html.P("This is the first paragraph of my HTML page"),
+            html.P("This is the second paragraph of my HTML page")
+        ]),
+        html.Div([
+            html.Img(src=app.get_asset_url('bar-chart-placeholder.png'))
+        ])
+    ])
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
